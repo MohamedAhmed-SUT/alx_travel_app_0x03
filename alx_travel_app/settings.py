bdf25da3d71bb8208bf1a1e65c3de0410057f6db
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-08-30 17:51:19 +0300

    update

---
+++ alx_travel_app/settings.py
@@ -0,0 +1,13 @@
+# Celery settings
+CELERY_BROKER_URL = "amqp://localhost"   # RabbitMQ broker
+CELERY_RESULT_BACKEND = "rpc://"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "your_email@gmail.com"
+EMAIL_HOST_PASSWORD = "your_password_or_app_password"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
